b0672e9bf8b45553d8a0f2f9c8d081ec81781d48
Tiago Conceição <Tiago_caza@hotmail.com>
2022-02-25 21:15:13 +0000

    Remove unused usings

--- src/Projektanker.Icons.Avalonia/Icon.axaml.cs
+++ src/Projektanker.Icons.Avalonia/Icon.axaml.cs
@@ -4,8 +4,6 @@ using Avalonia;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using Avalonia.Rendering;
 
 namespace Projektanker.Icons.Avalonia
 {
