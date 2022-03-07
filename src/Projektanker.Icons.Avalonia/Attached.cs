4f65751ab204c802bf1dddb44a8c55f188846867
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2022-03-07 13:41:14 +0100

    Updates attached documentation

--- src/Projektanker.Icons.Avalonia/Attached.cs
+++ src/Projektanker.Icons.Avalonia/Attached.cs
@@ -7,8 +7,9 @@ namespace Projektanker.Icons.Avalonia
     public static class Attached
     {
         /// <summary>
-        /// Identifies the FontAwesome.Avalonia.Awesome.Content attached dependency property.
+        /// Identifies the <seealso cref="IconProperty"/> avalonia attached property.
         /// </summary>
+        /// <value>Provide a icon identifier or binding.</value>
         public static readonly AttachedProperty<string> IconProperty =
             AvaloniaProperty.RegisterAttached<Icon, ContentControl, string>("Icon", string.Empty);
 
@@ -17,11 +18,21 @@ namespace Projektanker.Icons.Avalonia
             IconProperty.Changed.Subscribe(IconChanged);
         }
 
+        /// <summary>
+        /// Accessor for attached property <see cref="IconProperty"/>
+        /// </summary>
+        /// <param name="target">Target host</param>
+        /// <returns>The value of the <see cref="IconProperty"/></returns>
         public static string GetIcon(ContentControl target)
         {
             return target.GetValue(IconProperty);
         }
 
+        /// <summary>
+        /// Accessor for attached property <see cref="IconProperty"/>
+        /// </summary>
+        /// <param name="target">Target host</param>
+        /// <param name="value">The value to be set.</param>
         public static void SetIcon(ContentControl target, string value)
         {
             target.SetValue(IconProperty, value);
