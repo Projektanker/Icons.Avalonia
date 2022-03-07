72c114da8d04682cd4d3fe5edcc339ac01894218
Tiago Conceição <Tiago_caza@hotmail.com>
2022-03-07 18:17:26 +0000

    Remove NativeMenuItem support

--- src/Projektanker.Icons.Avalonia/Attached.cs
+++ src/Projektanker.Icons.Avalonia/Attached.cs
@@ -18,18 +18,10 @@ namespace Projektanker.Icons.Avalonia
         public static readonly AttachedProperty<string> MenuItemIconProperty =
             AvaloniaProperty.RegisterAttached<Icon, MenuItem, string>("Icon", string.Empty);
 
-        /// <summary>
-        /// Identifies the FontAwesome.Avalonia.Awesome.Content attached dependency property.
-        /// </summary>
-        public static readonly AttachedProperty<string> NativeMenuItemIconProperty =
-            AvaloniaProperty.RegisterAttached<Icon, NativeMenuItem, string>("Icon", string.Empty);
-
-
         static Attached()
         {
             ContentControlIconProperty.Changed.Subscribe(IconChanged);
             MenuItemIconProperty.Changed.Subscribe(IconChanged);
-            NativeMenuItemIconProperty.Changed.Subscribe(IconChanged);
         }
 
         public static string GetIcon(ContentControl target)
@@ -52,16 +44,6 @@ namespace Projektanker.Icons.Avalonia
             target.SetValue(MenuItemIconProperty, value);
         }
 
-        public static string GetIcon(NativeMenuItem target)
-        {
-            return target.GetValue(NativeMenuItemIconProperty);
-        }
-
-        public static void SetIcon(NativeMenuItem target, string value)
-        {
-            target.SetValue(NativeMenuItemIconProperty, value);
-        }
-
         private static void IconChanged(AvaloniaPropertyChangedEventArgs evt)
         {
             if (!(evt.NewValue is string value))
@@ -76,9 +58,6 @@ namespace Projektanker.Icons.Avalonia
                     break;
                 case MenuItem target:
                     target.Icon = MakeIcon(value);
-                    break;
-                case NativeMenuItem target:
-                    target.Icon = MakeIcon(value).BitmapImage;
                     break;
             }
         }
