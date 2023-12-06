63028db2c32ff274048c41ee03f55faf02c0f5fe
SebastianRumohr <sebastian.rumohr@projektanker.de>
2023-12-06 15:09:42 +0100

    refactor: Caches assembly of `MaterialDesignIconProvider`

--- src/Projektanker.Icons.Avalonia.MaterialDesign/MaterialDesignIconProvider.cs
+++ src/Projektanker.Icons.Avalonia.MaterialDesign/MaterialDesignIconProvider.cs
@@ -13,12 +13,14 @@ namespace Projektanker.Icons.Avalonia.MaterialDesign
     {
         private const string _mdiProviderPrefix = "mdi";
 
-        private static readonly string _resourceNameTemplate
-            = $"{typeof(MaterialDesignIconProvider).Assembly.GetName().Name}.Assets.{{0}}.svg";
+        private static readonly Assembly _assembly = typeof(MaterialDesignIconProvider).Assembly;
 
-        private static readonly Regex _viewBoxRegex = new Regex("viewBox=\"([0-9 -]+)\"");
-        private static readonly Regex _pathRegex = new Regex("<path d=\"(.+)\"");
-        private readonly Dictionary<string, IconModel> _icons = new Dictionary<string, IconModel>();
+        private static readonly string _resourceNameTemplate
+            = $"{_assembly.GetName().Name}.Assets.{{0}}.svg";
+
+        private static readonly Regex _viewBoxRegex = new("viewBox=\"([0-9 -]+)\"");
+        private static readonly Regex _pathRegex = new("<path d=\"(.+)\"");
+        private readonly Dictionary<string, IconModel> _icons = new();
 
         public string Prefix => _mdiProviderPrefix;
 
@@ -68,8 +70,7 @@ namespace Projektanker.Icons.Avalonia.MaterialDesign
 
             var withoutPrefix = value.Substring(_mdiProviderPrefix.Length + 1);
             var resourceName = string.Format(_resourceNameTemplate, withoutPrefix);
-            stream = Assembly.GetExecutingAssembly()
-                .GetManifestResourceStream(resourceName);
+            stream = _assembly.GetManifestResourceStream(resourceName);
 
             return stream != default;
         }
